3375062aad5f5d12b838af8a8eddb6db4f0ed25e
Elvis567890 <mbaziiraelvis0@gmail.com>
2026-09-03 22:37:51 +0300

    Update Puppeteer args to include '--disable-dev-shm-usage'
    Added '--disable-dev-shm-usage' argument to Puppeteer options.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new Client({
     puppeteer: {
         headless: true,
         executablePath: '/usr/bin/chromium',
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
     }
 });
 
